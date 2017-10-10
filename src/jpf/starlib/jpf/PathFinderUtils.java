c7a7bb78c300844f41884180da93bde63b822dfd
Quoc-Sang Phan <dark2bright@gmail.com>
2017-10-09 17:42:09 -0700

    remove redanduncy

--- src/jpf/starlib/jpf/PathFinderUtils.java
+++ src/jpf/starlib/jpf/PathFinderUtils.java
@@ -1,7 +1,5 @@
 package starlib.jpf;
 
-import java.util.List;
-
 import gov.nasa.jpf.vm.FieldInfo;
 import starlib.formula.Variable;
 
@@ -25,40 +23,6 @@ public class PathFinderUtils {
 		}
 
 		return false;
-	}
-	
-	public static void genDefaultVars(List<Variable> knownTypeVars, List<Variable> initVars,
-			StringBuffer test, String objName, String clsName,
-			FieldInfo[] insFields, FieldInfo[] staFields) {
-		if (knownTypeVars.size() == initVars.size())
-			return;
-		
-		for (Variable var : knownTypeVars) {
-			String name = var.getName();
-			String type = var.getType();
-			
-			if (name.startsWith("Anon_")) continue;
-				
-			if (!initVars.contains(var)) {
-				if (var.isPrim()) {
-					String val = type.equals("boolean") ? "false" : "0";
-					
-					if (isInstanceVariable(var,insFields))
-						test.append("\t\t" + name.replace("this_", objName + ".") + " = " + val + ";\n");
-					else if (isClassVariable(var,clsName, staFields))
-						test.append("\t\t" + name.replace(clsName + "_", clsName + ".") + " = " + val + ";\n");
-					else
-						test.append("\t\t" + type + " " + name + " = " + val + ";\n");
-				} else {
-					if (isInstanceVariable(var,insFields))
-						test.append("\t\t" + name.replace("this_", objName + ".") + " = null;\n");
-					else if (isClassVariable(var,clsName, staFields))
-						test.append("\t\t" + name.replace(clsName + "_", clsName + ".") + " = null;\n");
-					else
-						test.append("\t\t" + type + " " + name + " = null;\n");
-				}
-			}
-		}
 	}
 	
 	public static String standarizeName(Variable var, String objName, String clsName,
